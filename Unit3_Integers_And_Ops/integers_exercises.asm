d7f3d4f1833c78321c0714a489dd26434c8e044f
Megan Avery <mavery3@stedwards.edu>
2024-02-06 18:57:47 +0000

    integer representation lecture

--- Unit3_Integers_And_Ops/integers_exercises.asm
+++ Unit3_Integers_And_Ops/integers_exercises.asm
@@ -23,7 +23,35 @@ asm_main:
         enter   0,0               ; setup routine
         pusha
 
-		; TODO: add code
+        ; showcasing negative numbers
+        mov eax, -34
+        dump_regs 1
+        call print_nl
+
+        ; trying to sign extend a negative number incorrectly
+        mov eax, 0
+        mov al, 0DEH ; -34 into eax
+
+        call print_int
+        call print_nl
+
+        ; sign extending a negative number correctly
+        ; extend with 1s to the left
+        mov eax, 0FFFFFFFFH
+        mov al, 0DEH
+
+        call print_int
+        call print_nl
+        call print_nl
+
+        ; showing extending into a register with movzx
+        mov eax, -1
+        mov ax, 58
+        dump_regs 2
+        call print_nl
+
+        movzx eax, ax
+        dump_regs 3
 
         popa
         mov     eax, 0            ; return back to C
